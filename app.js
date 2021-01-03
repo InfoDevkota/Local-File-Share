2f873259e76c20a6c72826bf756a82baeb12e310
Sagar Devkota <info@devkotasagar.com.np>
2021-01-03 08:59:47 +0545

    local network interface address on log message

--- app.js
+++ app.js
@@ -50,3 +50,27 @@ app.post("/", upload.array("files", 12),  (req, res, next) =>{
 
 app.listen(5010);
 console.log("Local File Share Running on localhost:5010");
+
+const getIpAddresses = () =>{
+  const nets = require('os').networkInterfaces();
+  console.log("Looking for Local IP")
+  let found = false;
+  for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+          // if (net.family === 'IPv4' && !net.internal) {
+            if(net.address.startsWith('192.168.')){
+              console.log("IP Found " + net.address);
+              found = true;
+              console.log(`Access "Local File Share" at http://${net.address}:5010`);
+            }
+          // }
+      }
+  }
+  if(!found){
+    console.log("IP not detected.");
+    console.log("Verify you are connected to a network.")
+    console.log("Please use ipconfig/ifconfig");
+  }
+}
+
+getIpAddresses()
